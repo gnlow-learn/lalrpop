34cdec3157add55a7c4fe6713649bc3a29940b04
Gnlow <gnlow@protonmail.com>
2022-11-16 21:56:00 +0900

    calculator5

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,12 +1,12 @@
 #[macro_use] extern crate lalrpop_util;
 
-lalrpop_mod!(pub calculator4);
+lalrpop_mod!(pub calculator5);
 pub mod ast;
 
 #[test]
-fn calculator4() {
-    let expr = calculator4::ExprParser::new()
-        .parse("22 * 44 + 66").unwrap();
+fn calculator5() {
+    let expr = calculator5::ExprsParser::new()
+        .parse("22 * 44 + 66, 13 * 3").unwrap();
     println!("{:?}", expr);
-    assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
+    assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66), (13 * 3)]");
 }
