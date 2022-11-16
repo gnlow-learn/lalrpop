49801e957da15230ab3faea97a78ff37b2929eb9
Gnlow <gnlow@protonmail.com>
2022-11-16 17:09:21 +0900

    calculator3

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use] extern crate lalrpop_util;
-lalrpop_mod!(pub calculator1);
+lalrpop_mod!(pub calculator3);
 
-#[test]
-fn calculator1() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+use calculator3::ExprParser;
+
+//#[test]
+fn main() {
+    assert!(ExprParser::new().parse("22").is_ok());
+    assert!(ExprParser::new().parse("(22)").is_ok());
+    assert!(ExprParser::new().parse("((((22))))").is_ok());
+    assert!(ExprParser::new().parse("((22)").is_err());
+    println!("{:?}", ExprParser::new().parse("2 + 3 * 4"));
 }
