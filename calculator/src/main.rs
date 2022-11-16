3559b371a606d99cb10cd4548a6a84003d56d4e8
Gnlow <gnlow@protonmail.com>
2022-11-16 16:38:03 +0900

    init

---
+++ calculator/src/main.rs
@@ -0,0 +1,10 @@
+#[macro_use] extern crate lalrpop_util;
+lalrpop_mod!(pub calculator1);
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+}
